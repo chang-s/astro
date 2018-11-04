fd546081d6708dfdf93a5c1d647a50e4250227f1
Sora <40410828+chang-s@users.noreply.github.com>
2018-11-05 08:52:16 +0900

    Update ant.hpp
    Small reformatting

--- ant.hpp
+++ ant.hpp
@@ -1,25 +1,21 @@
 /*******************************************************************
-** Program Name:    CS 162 - Group Project - Predator-Prey Sim
-** Author:          Group 8
-** Date:            November 4, 2018
-** Description:     Header file for Ant class
+* Program Name:    CS 162 - Group Project - Predator-Prey Sim
+* Author:          Group 8 (Astro)
+* Date:            November 4, 2018
+* Description:     Header file for Ant class
 *******************************************************************/
 
 #ifndef ANT_HPP
 #define ANT_HPP
 
-#include "critter.hpp"
+#include "Critter.hpp"
 
 class Ant : public Critter {
-	private:
-
-	protected:
-
 	public:
-                // constructor that calls the Critter class constructor
+                // Constructor that calls the Critter class constructor
 		Ant(int row, int col, int critSteps = 0, int lastBreed = 0, int state = 0) 
 		: Critter(row, col, critSteps, lastBreed, state) {};
-                // empty destructor
+                // Empty destructor
 		~Ant() {}
 		virtual Critter* move(Direction dir) override;
 		virtual Critter* breed(Direction dir) override;
