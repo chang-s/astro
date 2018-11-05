2bd9eee4827f509fbeb4cc43a0a02450038b099a
Sora <40410828+chang-s@users.noreply.github.com>
2018-11-05 09:17:56 +0900

    Update critter.cpp

--- critter.cpp
+++ critter.cpp
@@ -1,17 +1,17 @@
 /*******************************************************************
-** Program Name:    CS 162 - Group Project - Predator-Prey Sim
-** Author:          Group 8
-** Date:            November 4, 2018
-** Description:     Source file for Critter class. This class is an
-**                  abstract class, and in the context of this
-**                  program has two derived classes: Ant and 
-**                  Doodlebug. These classes represent the prey and
-**                  predator, respectively, in this simulation.
+* Program Name:    CS 162 - Group Project - Predator-Prey Sim
+* Author:          Group 8 (Astro)
+* Date:            November 4, 2018
+* Description:     Source file for Critter class. This class is an
+*                  abstract class, and in the context of this
+*                  program has two derived classes: Ant and 
+*                  Doodlebug. These classes represent the prey and
+*                  predator, respectively, in this simulation.
 *******************************************************************/
 
 #include "critter.hpp"
 
-//Overloaded constructor
+// Overloaded constructor
 Critter::Critter(int row, int col, int critSteps, int lastBreed, int state) {
 	this->row = row;
 	this->col = col;
@@ -21,7 +21,7 @@ Critter::Critter(int row, int col, int critSteps, int lastBreed, int state) {
 	hasMoved = false;
 }
 
-//Empty destructor
+// Empty destructor
 Critter::~Critter() {
 
 }
